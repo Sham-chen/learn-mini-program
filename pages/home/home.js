97b147882fcf2d50461ecfbd9f71df3531ed8694
sham <396635291@qq.com>
2020-04-06 10:51:48 +0800

    事件处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,61 @@ Page({
    * 页面的初始数据
    */
   data: {
+    movies: ['千与千寻', '美丽心灵', '肖申克的救赎']
+  },
 
+  //方法
+  handleTouchStart() {
+    console.log('handleTouchStart');
+  },
+  handleTouchMove() {
+    console.log('handleTouchMove');
+  },
+  handleTouchEnd() {
+    console.log('handleTouchEnd');
+  },
+  handleTap() {
+    console.log('handleTap');
+  },
+  handleLongPress() {
+    console.log('handleLongPress');
+  },
+  handleEventClick(event) {
+    console.log('-----', event);
+  },
+  touchEnd(event) {
+    console.log('++++++', event);
+  },
+  handleInnerClick(event) {
+    console.log(event);
+  },
+  handleOuterClick(event) {
+    console.log(event);
+  },
+  handleItemClick(event) {
+    console.log(event);
+    const dataset = event.currentTarget.dataset
+    const item = dataset.item
+    const index = dataset.index
+    console.log(item, index);
+  },
+  handleCaptureView1() {
+    console.log('handleCaptureView1');
+  },
+  handleBindView1() {
+    console.log('handleBindView1');
+  },
+  handleCaptureView2() {
+    console.log('handleCaptureView2');
+  },
+  handleBindView2() {
+    console.log('handleBindView2');
+  },
+  handleCaptureView3() {
+    console.log('handleCaptureView3');
+  },
+  handleBindView3() {
+    console.log('handleBindView3');
   },
 
   /**
